37aa331229a430be447764d229d5f1c043593897
Jenny Liu <jenny.liu.ca@gmail.com>
2023-01-19 21:08:21 -0800

    add conftest.py

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,6 +1,25 @@
 import hello
 
+import pytest, sys
+import logging
 
-def test_hello_name():
-    assert "bob" in hello.hello_name("bob")
-    assert "bob" not in hello.hello_name("not")
+# logging.basicConfig(
+#     level=logging.DEBUG,
+#     format='%(levelname)-8s | %(name)s | %(asctime)s | %(module)s:%(lineno)-4d | %(message)s'
+#     )
+
+logger = logging.getLogger("unit test")
+
+def process(name, check_string):
+    logger.info(f'processing arg name : {name}')    
+    logger.info(f'processing arg check_string : {check_string}')    
+    return check_string in hello.hello_name(name)
+
+@pytest.mark.TEST00001
+@pytest.mark.parametrize("dstring, eachtest_umbcld_setupteardown, name, check_string, expected_result", [
+    ("TEST00001 : test hello_name()", ["setup_arg"], "bob", "bob", True),
+    ("TEST00002 : test hello_name()", ["setup_arg"], "bob", "haha", False),
+], indirect=["eachtest_umbcld_setupteardown"])
+def test_hello_name(suite_dashwebsetupteardown, eachtest_umbcld_setupteardown, dstring, name, check_string, expected_result):
+    test_hello_name.__doc__ = dstring
+    assert process(name, check_string) == expected_result
