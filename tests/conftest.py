86d8c21dbdcdbf888ff98a31989aa960d86d3ee6
Jenny Liu <jenny.liu.ca@gmail.com>
2023-01-20 10:06:35 -0800

    add install black

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 import sys
-sys.path.append('.')
-sys.path.append('tests')
+
+sys.path.append(".")
+sys.path.append("tests")
 
 import logging
 import pytest
 
 
-
 logger = logging.getLogger("unit test")
+
 
 @pytest.fixture(scope="module")
 def suite_dashwebsetupteardown():
@@ -18,12 +19,13 @@ def suite_dashwebsetupteardown():
 
     logger.info("suite_dashwebsetupteardown fixture end...")
 
+
 @pytest.fixture
 def eachtest_umbcld_setupteardown(request):
     logger.info("eachtest_umbcld_setupteardown fixture start...")
-    
+
     arg_1 = request.param[0]
-    logger.info(f'setup arg : {arg_1}')
+    logger.info(f"setup arg : {arg_1}")
 
     yield
 
--- tests/start_tests.py
+++ tests/start_tests.py
@@ -1,33 +1,82 @@
 import sys
 
-sys.path.append('.')
-sys.path.append('lib')
-sys.path.append('tests')
-sys.path.append('tests/lib')
+sys.path.append(".")
+sys.path.append("lib")
+sys.path.append("tests")
+sys.path.append("tests/lib")
 
 import argparse
 import subprocess
 import os
 
+
 def run_tests_with_scenarios(scenarios, testmarker, htmlOutput, basetestpath):
-    if 'win32' in sys.platform:
-        pytest_command = 'python -m pytest '
+    if "win32" in sys.platform:
+        pytest_command = "python -m pytest "
     else:
         # used in 'darwin'
-        pytest_command = 'python3 -m pytest '
+        pytest_command = "python3 -m pytest "
 
     if testmarker:
         if htmlOutput == "YES":
-            command = pytest_command + ' --capture=sys --html=' + basetestpath + os.path.sep + 'pytest_report.html --self-contained-html --junitxml=' + basetestpath + os.path.sep + 'pytest_report.xml ' + basetestpath + os.path.sep + scenarios + ' -v -k "' + testmarker
+            command = (
+                pytest_command
+                + " --capture=sys --html="
+                + basetestpath
+                + os.path.sep
+                + "pytest_report.html --self-contained-html --junitxml="
+                + basetestpath
+                + os.path.sep
+                + "pytest_report.xml "
+                + basetestpath
+                + os.path.sep
+                + scenarios
+                + ' -v -k "'
+                + testmarker
+            )
         else:
-            command = pytest_command + ' -s --junitxml=' + basetestpath + os.path.sep + 'pytest_report.xml ' + basetestpath + os.path.sep + scenarios + ' -v -k "' + testmarker
+            command = (
+                pytest_command
+                + " -s --junitxml="
+                + basetestpath
+                + os.path.sep
+                + "pytest_report.xml "
+                + basetestpath
+                + os.path.sep
+                + scenarios
+                + ' -v -k "'
+                + testmarker
+            )
         print(command)
         res = subprocess.call(command, shell=True)
     else:
         if htmlOutput == "YES":
-            command = pytest_command + ' --capture=sys --html=' + basetestpath + os.path.sep + 'pytest_report.html --self-contained-html --junitxml=' + basetestpath + os.path.sep + 'pytest_report.xml ' + basetestpath + os.path.sep + scenarios + ' -v'
+            command = (
+                pytest_command
+                + " --capture=sys --html="
+                + basetestpath
+                + os.path.sep
+                + "pytest_report.html --self-contained-html --junitxml="
+                + basetestpath
+                + os.path.sep
+                + "pytest_report.xml "
+                + basetestpath
+                + os.path.sep
+                + scenarios
+                + " -v"
+            )
         else:
-            command = pytest_command + ' -s --junitxml=' + basetestpath + os.path.sep + 'pytest_report.xml ' + basetestpath + os.path.sep + scenarios + ' -v'
+            command = (
+                pytest_command
+                + " -s --junitxml="
+                + basetestpath
+                + os.path.sep
+                + "pytest_report.xml "
+                + basetestpath
+                + os.path.sep
+                + scenarios
+                + " -v"
+            )
         print(command)
         res = subprocess.call(command, shell=True)
     return res
@@ -38,22 +87,30 @@ def main():
     htmlOutput = "YES"
 
     parser = argparse.ArgumentParser()
-    requiredNamed = parser.add_argument_group('required named arguments')
-    parser.add_argument("--testmarker", "-t", default=None, help="Pass marker to run specific group of tests")
-    parser.add_argument("--htmlOutput", default="YES", help="NO for debugging - output will go to stdout and summary will be unavailable")
-    
+    requiredNamed = parser.add_argument_group("required named arguments")
+    parser.add_argument(
+        "--testmarker",
+        "-t",
+        default=None,
+        help="Pass marker to run specific group of tests",
+    )
+    parser.add_argument(
+        "--htmlOutput",
+        default="YES",
+        help="NO for debugging - output will go to stdout and summary will be unavailable",
+    )
+
     args = parser.parse_args()
 
     testmarker = args.testmarker
     htmlOutput = args.htmlOutput
-    
+
     basetestpath = os.getcwd()
-    
-    scenarios = 'test_hello.py'
-    
+
+    scenarios = "test_hello.py"
+
     run_tests_with_scenarios(scenarios, testmarker, htmlOutput, basetestpath)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
