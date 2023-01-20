f70480308cba7ed8b0b52bac872450769f77f418
Jenny Liu <jenny.liu.ca@gmail.com>
2023-01-20 14:38:08 -0800

    add report column description

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,10 +6,38 @@ sys.path.append("tests")
 
 import logging
 import pytest
+from py.xml import html
+
 
 
 logger = logging.getLogger("unit test")
 
+
+def pytest_configure(config):
+    config._metadata['test software version'] = '0.1'
+
+@pytest.hookimpl(optionalhook=True)
+def pytest_html_results_table_header(cells):
+    cells.insert(1, html.th('Description'))
+    cells.pop()
+
+@pytest.hookimpl(optionalhook=True)
+def pytest_html_results_table_row(report, cells):
+    try:
+        cells.insert(1, html.td(report.description))
+    except AttributeError as error:
+        logger.info("AttributeError trying to pytest_html_results_table_row - start")
+        logger.error(error)
+        logger.info("AttributeError trying to pytest_html_results_table_row - end")
+    except Exception as e:
+        logger.error(f"Unknown Exception trying to pytest_html_results_table_row : {e}")
+    cells.pop()
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+    report.description = str(item.function.__doc__)
 
 @pytest.fixture(scope="module")
 def suite_setupteardown():
